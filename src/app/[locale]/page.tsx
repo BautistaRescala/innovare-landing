37db30e4efeae1ee68aa0ed10add688c4808af6a
BautisaRescala <bautistarescala@gmail.com>
2024-10-07 03:16:23 -0300

    First HomePage div

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -10,14 +10,16 @@ export default function HomePage({params: {locale}}:{params: {locale:string}}) {
   const t = useTranslations('HomePage');
   return (
     <div className='mt-20'>
-      <div  className='flex pl-10 justify-between h-[1200px]'>
+      <div  className='flex pl-10 justify-between'>
         <p className={`${contentfont.className} pt-16 max-w-[680px] text-6xl z-10`}
         >{t.rich("applications",{br:() => <br></br>})}</p>
-        <div className="flex-none relative z-10 before:absolute before:w-full before:h-full before:bg-gradient-to-l from-transparent to-white">
-        <Image src="/homepage1.jpg" alt="officePhoto" width={400} height={585}
-        className=""></Image>
-        </div>
+        <div className="hidden md:block flex-none relative z-10 before:absolute before:w-full before:h-full before:bg-gradient-to-b from-transparent from-0% to-98% to-white">
+          <div className="flex-none z-15 absolute w-full h-full bg-gradient-to-r from-white to-transparent from-0% to-30%"></div>
+          <Image src="/homepage1.jpg" alt="officePhoto" width={520} height={676}
+          className=""></Image>
+          </div>
       </div>
+      <div className="h-[1200px] bg-pink-600"></div>
     </div>
   );
 }
